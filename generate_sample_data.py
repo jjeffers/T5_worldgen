c24da6e737a45407e72e23760cf4b21fd4498205
Arthur Green <arthur@phraction.org>
2017-06-09 00:40:00 +0100

    Add sample data script

---
+++ generate_sample_data.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+'''
+Mega-system generator
+'''
+
+from T5_worldgen.system import System
+
+print 'name,port,siz,atm,hyd,pop,gov,law,tl,uwp'
+for ctr in range(5000):
+    syst = System('System-{:04d}'.format(ctr))
+    print ','.join([
+        syst.name,
+        str(syst.mainworld.starport),
+        str(syst.mainworld.size),
+        str(syst.mainworld.atmosphere),
+        str(syst.mainworld.hydrographics),
+        str(syst.mainworld.population),
+        str(syst.mainworld.government),
+        str(syst.mainworld.law_level),
+        str(syst.mainworld.tech_level),
+        syst.mainworld.uwp()
+    ])
