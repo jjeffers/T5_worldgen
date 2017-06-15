f4274eed9405ebd306f8efb32340bdf5c07af14f
Arthur Green <arthur@phraction.org>
2017-06-15 15:45:38 +0100

    Use Sector t5_tab method for output, write json for subsectors

--- gen_sector.py
+++ gen_sector.py
@@ -1,6 +1,26 @@
 #! /usr/bin/env python
 
 from T5_worldgen.mapping_region import Sector
+import os
 
-sector = Sector('Test Sector', 'Sparse')
-sector.display()
+sector_name = 'Test_Sector'
+
+sector = Sector(sector_name, 'Scattered')
+# sector.display()
+for _ in sector.t5_tab():
+    print _
+
+# Save JSON data in ./tmp/sector_name/subsector_name.json
+try:
+    os.makedirs('./tmp/{}/'.format(sector_name))
+except OSError:
+    if not os.path.isdir('./tmp/{}/'.format(sector_name)):
+        raise
+for ss_id in sector.subsectors.keys():
+    with open(
+            './tmp/{}/Subsector-{}.json'.format(sector_name, ss_id),
+            'w') as jout:
+        for hex_id in sector.subsectors[ss_id].hexes.keys():
+            jout.write(
+                sector.subsectors[ss_id].hexes[hex_id].as_json() + "\n"
+            )
